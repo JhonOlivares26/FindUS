744d2f6dc0409eb5523ebc533243777bfcefbb43
JhonOlivares26 <89679333+JhonOlivares26@users.noreply.github.com>
2025-02-06 18:46:26 -0500

    feat: add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import Home from './features/home/home';
 import './index.css';
 
 const App = () => {
@@ -7,10 +9,10 @@ const App = () => {
     <Router>
       <Routes>
         {/* Redirigir desde la raíz a la página de login */}
-        <Route path="/" element={<Navigate to="/login" />} />
-      
+        <Route path="/" element={<Navigate to="/login" />} />  
         <Route path="/login" sensitive={true} element={<LoginPage />} />
-        {/* Agrega más rutas aquí según sea necesario */}
+        <Route path="/register" sensitive={true} element={<RegisterPage />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
